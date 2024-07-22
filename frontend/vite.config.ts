8fbcf17fa39eca08b3b97081824f12d13c9503af
herinpatel15 <herinpatel15113@gmail.com>
2024-07-22 20:34:26 +0530

    shadcn setup complet

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3030,
+    proxy: {
+      '/api/v1/restaurants/': {
+        target: 'http://localhost:8080',
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
