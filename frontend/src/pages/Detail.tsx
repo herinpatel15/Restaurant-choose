384f618e84ef7dd03179377825dc753d18c437e1
herinpatel15 <herinpatel15113@gmail.com>
2024-07-27 23:06:59 +0530

    basic rating component

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -1,13 +1,19 @@
 import { useParams } from "react-router-dom"
-
+import {Rating, Star} from "@/components/ui/Rating"
 
 export default function Detail() {
 
     const id = useParams().id
 
     return (
-        <main>
-            <h1>detail {id}</h1>
+        <main className="bg-black h-screen flex flex-col items-center justify-center">
+            <Rating className="text-6xl text-blue-600">
+                <Star />
+                <Star />
+                <Star />
+                <Star />
+                <Star />
+            </Rating>
         </main>
     )
 }
